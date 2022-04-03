b14a86ff86499c04200a76893dace46977c07b7f
Nina Quick <ninaquick@Ninas-MacBook-Pro.local>
2022-04-02 21:43:33 -0700

    add t.tests for PSI against population mean of 1,500

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-# Load MechcCar_mpg.csv file into a dataframe
+# Load MechaCar_mpg.csv file into a dataframe
 mechaCar <- read.csv("MechaCar_mpg.csv")
 #head(mechaCar)
 
@@ -13,8 +13,15 @@ head(suspensionCoil)
 
 #Summary for suspension coil's PSI column
 total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
-head(total_summary)
+#head(total_summary)
 
 # Summary of PSI column by manufacturing lot
-
 lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
+
+# Use T-Test to see if the PSI across all lots is statistically different from a population mean of 1,500
+t.test(suspensionCoil$PSI, mu=1500)
+
+# Use T-Test for each lot to see if they are statistically different from a population mean of 1,500
+t.test(subset(suspensionCoil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(suspensionCoil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(suspensionCoil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
